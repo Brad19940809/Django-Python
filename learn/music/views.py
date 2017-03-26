a91042d5d0fe293a3fc725651f309ed48999fa7f
Brad <xiaoleluo2@gmail.com>
2017-03-26 12:02:42 +0800

    the view has used a shortcut, and some details have been added too.

--- learn/music/views.py
+++ learn/music/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from music.models import Album
 from django.template import loader
 
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('music/index.html')
     context = {
         'all_albums': all_albums,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'music/index.html', context)
 
 
 def detail(request, album_id):
-    return HttpResponse('<h2>Details for Album id:' + album_id+'</h2>')
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album': album})
